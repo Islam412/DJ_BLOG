6dcd2c7729f44016195396428f85e5d472e69950
Islam412 <islam.124.hamdy@gmail.com>
2023-09-06 04:15:06 +0300

    desing model 1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content= models.TextField(max_length=30000)
+    create_date= models.DateTimeField(default=timezone.now())
+    draft = models.BooleanField(default=True)
+    tags = TaggableManager()
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='post_user')
+    img =models.ImageField(upload_to='posts/',null=True)
+
+    def __str__(self):
+        return self.title
