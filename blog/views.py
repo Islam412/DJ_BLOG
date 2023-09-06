e9c266e8445754eca3e78a0e4709c4d14571b811
Islam412 <islam.124.hamdy@gmail.com>
2023-09-06 05:23:44 +0300

    show all post , add url post into template

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,9 @@ from .models import Post
 
 def Post_List(request):
     data = Post.objects.all()
-    return render(request , 'post_list.html' ,{})
+    return render(request , 'post_list.html' ,{'posts':data})
+
+
+def Post_Detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+    return render(request,'post_detail.html',{'post':data})
--- project/urls.py
+++ project/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import Post_List
+from blog.views import Post_List ,Post_Detail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/' , Post_List)
+    path('blog/' , Post_List),
+    path('blog/<int:post_id>' , Post_Detail),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
