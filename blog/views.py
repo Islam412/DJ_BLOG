755f8d0445c1a43e82ae5938f0dd2bb4ee9082df
Islam412 <islam.124.hamdy@gmail.com>
2023-09-06 04:46:41 +0300

    add views , model , url project and app , add template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def Post_List(request):
+    data = Post.objects.all()
+    return render(request , 'post_list.html' ,{})
--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import Post_List
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/' , Post_List)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
