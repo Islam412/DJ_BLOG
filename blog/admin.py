458132d092fe7e77c6fed1573a6694f94f8550aa
Islam412 <islam.124.hamdy@gmail.com>
2023-09-07 02:07:53 +0300

    edite space admin banal

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,17 @@ from .models import Post , Comment
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = '__all__'
+    list_display = ['title','draft','author']
+    list_filter = ['draft','author']
+    search_fields = ['title','content']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user','post','create_at']
+    list_filter = ['user']
 
 # Register your models here.
 admin.site.register(Post,PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment,CommentAdmin)
+admin.site.site_header = 'BLOG'
+admin.site.site_title = 'BLOG'
