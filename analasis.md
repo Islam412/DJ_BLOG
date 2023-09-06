321f2d99d53081b988dadac647ca2932a57dae92
Islam412 <islam.124.hamdy@gmail.com>
2023-09-06 03:34:22 +0300

    add blog app & analsis.md

---
+++ analasis.md
@@ -0,0 +1,15 @@
+post
+    -title
+    -author
+    -content
+    -img
+    -tag
+    -creat_date
+    -draft
+
+
+comment
+    -creat_date
+    -comment
+    -post
+    -author
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -122,7 +122,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR / "media"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
