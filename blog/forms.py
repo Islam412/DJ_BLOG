2a2898eefea1a93305d1b9c4f9e8dc581d9ae7d0
Islam412 <islam.124.hamdy@gmail.com>
2023-09-07 06:52:32 +0300

    add new template

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Post , Comment
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('author',)
+        widgets = {
+            'content ': SummernoteWidget(),
+            'content': SummernoteInplaceWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
