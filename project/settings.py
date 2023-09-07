0abcd184789f139edef860ce1dd191a503ad9964
Islam412 <islam.124.hamdy@gmail.com>
2023-09-07 06:27:35 +0300

    add bootstrap frontend for post detail , add django bootstrap5 for comment

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
-    'django_summernote', 
+    'django_summernote',
+    'django_bootstrap5',
     'blog',
 ]
 
